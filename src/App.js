b277c1c3b3b0f179e059ba9d6239b2ceff573f4e
nusrat-jahan <njnusrat53@gmail.com>
2020-11-27 16:16:52 +0600

    initialize create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h2>I am from App</h2>
+        </div>
+    )
+}
+
+export default App
